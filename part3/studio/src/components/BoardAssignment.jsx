b278ccf84ee2c2be9d50259350ecd751236c7120
elenamayag <142683768+elenamayag@users.noreply.github.com>
2023-12-07 19:38:44 -0600

    part 3 react

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,15 +1,36 @@
-import { useState } from 'react';
+import { useState } from 'react'; 
 
 export default function BoardAssignment () {
-   const boards = [];
+
+   const [boardName, setBoardName] = useState("No boards yet!");
+   const boards = [
+      {
+         label: "Recipes to try!",
+         value: "newRecipes"
+      },
+      {
+         label: "Soups and others",
+         value: "soups"  
+      },
+      {
+         label: "Dinner Ideas",
+         value: "dinnerIdeas"
+      }
+   ];
+
+   let boardOptions = boards.map((board, index) => {
+      return <option key={index} value={board.vaule}>{board.value}</option>;
+   });
 
    const handleChange = (event) => {
+      setBoardName(event.target.value); 
    }
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+         {boardOptions}
       </select>
 
       <p>Saved to {boardName}!</p>
--- part3/studio/src/components/Recipe.jsx
+++ part3/studio/src/components/Recipe.jsx
@@ -1,32 +1,38 @@
 import { useState } from 'react';
 
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://www.allrecipes.com/lazy-hazy-beef-chili-recipe-7562950";
+   let authorPhoto = "https://www.allrecipes.com/thmb/rI3ImaTYU8HVtagwqw5HL2v1Tcs=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/chef-john-c50f1c2cb23f4e7786d66c30bddf8bcb.jpg";
+   let authorName = 'John Mitzewich';
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img
+            src={authorPhoto}
+            alt=""
+            style={{ objectFit: "contain", borderRadius: "50%" }}
+            width="200px"
+         />
          <div>
             <h3>{authorName}</h3>
-            <a href={authorLink}></a> 
+            <a href={authorLink}></a>
          </div>
       </div>
    );
 }
 
+
+
 const RecipeIngredients = () => {
-   const ingredients = [];
-   return(
+   const ingredients = ["2 pounds ground beef", "2 tablespoons chili powder", "12 ounces hazy IPA beer", "1 (14.5 ounce) can diced fire-roasted tomatoes with green chilies", "2 (16 ounce jars) extra chunky salsa" , "2 (15 ounce) cans pinto beans, drained"];
+   let ingredientListItems = ingredients.map((ingredient, index) => {
+      return <li key={index}>{ingredient} </li>;
+   })
+   return (
       <div>
          <h3>Recipe Ingredients</h3>
          <ul>
-            <li>{ingredients[0]}</li>
-            <li>{ingredients[1]}</li>
-            <li>{ingredients[2]}</li>
-            <li>{ingredients[3]}</li>
-            <li>{ingredients[4]}</li>
+            {ingredientListItems}
          </ul>
       </div>
    );
@@ -34,10 +40,10 @@ const RecipeIngredients = () => {
 
 const RecipeDescription = () => {
    return (
-      <div> 
+      <div>
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Chili</h1>
+            <p>What is better then a good bowl of soup on a fall day? ... A BOWL OF CHILI duhhhh!</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -49,12 +55,15 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://www.allrecipes.com/lazy-hazy-beef-chili-recipe-7562950"
+         alt=""
+         className="imageUpdates"
+         height="450px" />
    );
 }
 
-export default function RecipeDisplay () {
-   return(
+export default function RecipeDisplay() {
+   return (
       <div className="recipePhotoBlock">
          <RecipePhoto />
          <div>
